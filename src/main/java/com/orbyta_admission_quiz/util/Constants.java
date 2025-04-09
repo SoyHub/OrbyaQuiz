e6b5b016137ce12117a660ede77ebe2250ec59cd
Sohayb <Sohayb.MuhammedTaherAliEweis-ESTERNI@cagroupsolutions.it>
2025-04-09 02:37:51 +0200

    chore: remove outdated API endpoint constants from Constants.java

--- src/main/java/com/orbyta_admission_quiz/util/Constants.java
+++ src/main/java/com/orbyta_admission_quiz/util/Constants.java
@@ -8,10 +8,6 @@ public class Constants {
     public static final String API_BASE_PATH = "/api/v1";
     public static final String ACCOUNT_PATH = "/accounts";
 
-    public static final String FABRICK_ACCOUNT_BALANCE_ENDPOINT = "/api/gbs/banking/v4.0/accounts/{accountId}/balance";
-    public static final String FABRICK_ACCOUNT_TRANSACTIONS_ENDPOINT = "/api/gbs/banking/v4.0/accounts/{accountId}/transactions";
-    public static final String FABRICK_MONEY_TRANSFER_ENDPOINT = "/api/gbs/banking/v4.0/accounts/{accountId}/payments/money-transfers";
-
     public static final String AUTH_SCHEMA_HEADER = "Auth-Schema";
     public static final String API_KEY_HEADER = "Api-Key";
     public static final String X_TIME_ZONE_HEADER = "X-Time-Zone";
