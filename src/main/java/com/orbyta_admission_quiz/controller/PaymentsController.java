540078c3733b5553be71a7aac1c0600058b67400
Sohayb <Sohayb.MuhammedTaherAliEweis-ESTERNI@cagroupsolutions.it>
2025-04-09 23:43:46 +0200

    feat: update PaymentsController with Swagger annotations for money transfer endpoint

--- src/main/java/com/orbyta_admission_quiz/controller/PaymentsController.java
+++ src/main/java/com/orbyta_admission_quiz/controller/PaymentsController.java
@@ -5,6 +5,11 @@ import com.orbyta_admission_quiz.dto.payments.request.MoneyTransferRequest;
 import com.orbyta_admission_quiz.dto.payments.response.MoneyTransferResponse;
 import com.orbyta_admission_quiz.service.PaymentsService;
 import com.orbyta_admission_quiz.util.Constants;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,11 +22,17 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping(Constants.API_BASE_PATH + Constants.ACCOUNT_PATH)
 @RequiredArgsConstructor
-@Tag(name = "Account Controller", description = "Controller for managing account operations")
+@Tag(name = "Payments Controller", description = "Controller for managing payments operations")
 public class PaymentsController {
 
     private final PaymentsService paymentsService;
 
+    @Operation(summary = "Create a money transfer", description = "Creates a money transfer for the specified account.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Money transfer created successfully", content = @Content(schema = @Schema(implementation = MoneyTransferResponse.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request data", content = @Content),
+            @ApiResponse(responseCode = "500", description = "Internal server error", content = @Content)
+    })
     @PostMapping("/{accountId}/payments/money-transfers")
     public ResponseEntity<Object> createMoneyTransfer(@PathVariable(required = false) Long accountId, @Valid @RequestBody MoneyTransferRequest request) {
         log.info("Creating money transfer for account: {} to {}", accountId, request.getCreditor().getName());
